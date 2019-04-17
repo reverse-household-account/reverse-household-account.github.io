57811e51a5169fdc44b748cd02bbde44c9aeed96
Leo.fp <Leo.fp@kakaocorp.com>
2019-04-17 11:27:42 +0900

    Updates

---
+++ precache-manifest.b83bf5dc6a84c5338b87ca2c8535671c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a2ace678f59a29c1c5a",
+    "url": "/static/js/main.6d594818.chunk.js"
+  },
+  {
+    "revision": "48578a3ca441958905a4",
+    "url": "/static/js/2.99b8ffb5.chunk.js"
+  },
+  {
+    "revision": "7a2ace678f59a29c1c5a",
+    "url": "/static/css/main.8713da48.chunk.css"
+  },
+  {
+    "revision": "48578a3ca441958905a4",
+    "url": "/static/css/2.3fec945a.chunk.css"
+  },
+  {
+    "revision": "190275ede0662bd0c864686ce0266dc5",
+    "url": "/index.html"
+  }
+];
